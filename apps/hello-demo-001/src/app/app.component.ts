c6f9301bbc5b10912be4a7e4e6a12e938f91c023
Charlie Greenman <charlie@razroo.com>
2023-10-26 20:26:57 -0400

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/hello-demo-001/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-demo-001-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hello-demo-001';
+}
